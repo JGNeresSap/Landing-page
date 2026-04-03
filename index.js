5dcab07f6cdb024b29a357ad6b697906905fb403
JGNeresSap <joaogabrielgcneres@gmail.com>
2026-04-03 13:11:14 -0300

    Refactor index.js for improved readability

--- index.js
+++ index.js
@@ -24,53 +24,57 @@ toggleBtn.addEventListener("click", function () {
     menuList.classList.toggle('menu-open');
     toggleBtn.classList.toggle('active');
 });
-const sr = ScrollReveal({
-    reset: false,
-    distance: '60px',
-    duration: 700,
-    easing: 'ease-out'
-});
-sr.reveal('.reveal-tittle', { origin: 'top' });
-sr.reveal('.reveal-subtittle', { delay: 200 });
-sr.reveal('.reveal-img', { origin: 'left', delay: 200, scale: 0.95 });
-sr.reveal('.reveal-txt', { origin: 'right', delay: 400, scale: 0.95 });
-sr.reveal('.card', {
-    interval: 200,
-    scale: 0.95
-});
-sr.reveal('.Causas-card', {
-    interval: 200,
-    scale: 0.95
-});
-sr.reveal('.duvida-box', {
-    interval: 150,
-    origin: 'bottom'
-});
-sr.reveal('.reveal-img2', {
-    origin: 'right',
-    delay: 200,
-    scale: 0.95
-});
-sr.reveal('.reveal-contato-left', {
-    origin: 'left',
-    delay: 200
-});
-sr.reveal('.reveal-contato-right', {
-    origin: 'right',
-    delay: 400
-});
-sr.reveal('.reveal-container', {
-    delay: 100
-});
-const links = document.querySelectorAll('.menu-container a');
-window.addEventListener('scroll', () => {
-    let fromTop = window.scrollY + 100;
-    links.forEach(link => {
-        let section = document.querySelector(link.getAttribute('href'));
-        if (section && section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
+    const sr = ScrollReveal({
+        reset: false,
+        distance: '60px',
+        duration: 700,
+        easing: 'ease-out'
     });
-});
+    sr.reveal('.reveal-tittle', { origin: 'top' });
+    sr.reveal('.reveal-subtittle', { delay: 200 });
+    sr.reveal('.reveal-img', { origin: 'left', delay: 200, scale: 0.95 });
+    sr.reveal('.reveal-txt', { origin: 'right', delay: 400, scale: 0.95 });
+    sr.reveal('.card', {
+        interval: 200,
+        scale: 0.95
+    });
+    sr.reveal('.Causas-card', {
+        interval: 200,
+        scale: 0.95
+    });
+    sr.reveal('.duvida-box', {
+        interval: 150,
+        origin: 'bottom'
+    });
+    sr.reveal('.reveal-img2', {
+        origin: 'right',
+        delay: 200,
+        scale: 0.95
+    });
+    sr.reveal('.reveal-contato-left', {
+        origin: 'left',
+        delay: 200
+    });
+    sr.reveal('.reveal-contato-right', {
+        origin: 'right',
+        delay: 200
+    });
+    sr.reveal('.reveal-contato-bottom', {
+        origin: 'bottom',
+        delay: 400
+    });
+    sr.reveal('.reveal-container', {
+        delay: 100
+    });
+    const links = document.querySelectorAll('.menu-container a');
+    window.addEventListener('scroll', () => {
+        let fromTop = window.scrollY + 100;
+        links.forEach(link => {
+            let section = document.querySelector(link.getAttribute('href'));
+            if (section && section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
